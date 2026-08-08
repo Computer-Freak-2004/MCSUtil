eb8473ed04bf37bd2d2f5afdb07ad1421442382c
Computer-Freak-2004 <89689115+Computer-Freak-2004@users.noreply.github.com>
2026-08-08 14:15:36 +0200

    deleteSingleDir: allow to show items of dir

--- src/deleteSingleDir.c
+++ src/deleteSingleDir.c
@@ -1,3 +1,5 @@
+#include "deleteSingleDir.h"
+
 #include <fxcg/display.h>
 #include <fxcg/keyboard.h>
 #include <fxcg/misc.h>
@@ -94,7 +96,7 @@ void deleteSingleDir() {
         PrintMini(&x, &y, buffer, 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);  // Dir Num
 
         x = 0, y += 20;
-        PrintMini(&x, &y, "[EXE] Delete Entry | [<] [>] Browse", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
+        PrintMini(&x, &y, "[F1] Items | [EXE] Delete | [<][>] Browse", 0, 0xFFFFFFFF, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
 
         GetKey(&key);
 
@@ -109,7 +111,7 @@ void deleteSingleDir() {
             if (current < MCS_SIZE)
                 current++;
         }
-        if (key == KEY_CTRL_EXE) {
+        if (key == KEY_CTRL_EXE) {  // Delete item
             MsgBoxPush(3);
             int confirm = 0;
             while (1) {
@@ -138,15 +140,99 @@ void deleteSingleDir() {
                     }
                     MCS_DeleteDirectory(dir->name);  // delete empty dir now
                 } else if (rc == 0x46) {
-                    errorMsg("Error 0x46!", "System dir can't be deleted.");
+                    errorMsg("Error 0x46:", "System dir can't be deleted.");
                 } else if (rc == 0xF0) {
-                    errorMsg("Error 0xF0!", "Dir is empty.");
+                    errorMsg("Error 0xF0:", "Directory is empty.");
                 } else if (rc == 0x40) {
-                    errorMsg("Error 0x40!", "Dir doesn't exist.");
+                    errorMsg("Error 0x40:", "Directory doesn't exist.");
                 } else if (rc == 0x47) {
-                    errorMsg("Error 0x47!", "Certain dir flags are set.");
+                    errorMsg("Error 0x47:", "Certain dir flags are set.");
                 }
             }
         }
+        if (key == KEY_CTRL_F1) {  // Show items
+            if (dir->count == 0) {
+                errorMsg("Error: ", "No items in this directory.");
+                continue;
+            }
+
+            // Get items
+            TItemEntry* items = (TItemEntry*)dir->addr;
+            ItemDisplay item_display[dir->count];
+            for (int i = 0; i < dir->count; i++) {
+                memcpy(item_display[i].name, items[i].name, 8);
+                item_display[i].name[8] = '\0';
+
+                char len_str[5];
+                itoa(items[i].length, (unsigned char*)len_str);
+                memcpy(item_display[i].length, len_str, 5);
+                item_display[i].length[5] = '\0';
+
+                char flag_str[12];
+                for (int i = 0; i < 4; i++) {
+                    ByteToHex((unsigned char)dir->flags[i], (unsigned char*)&flag_str[i * 3]);  // write 2 hex chars to 0, 3, 6, ...
+                    flag_str[(i * 3) + 2] = ' ';                                                // add space after 2 hex chars
+                }
+                flag_str[11] = '\0';
+                memcpy(item_display[i].flags, flag_str, 12);
+            }
+
+            int total_lines = dir->count;
+            int visible_lines = 5;
+            int scroll_pos = 0;
+            struct scrollbar sb;
+            sb.I1 = 0;
+            sb.indicatormaximum = total_lines;
+            sb.indicatorheight = visible_lines;
+            sb.indicatorpos = scroll_pos;
+            sb.I5 = 0;
+            sb.barleft = 342;
+            sb.bartop = 70;
+            sb.barheight = 95;
+            sb.barwidth = 6;
+
+            Scrollbar(&sb);
+
+            int scroll = 0;
+
+            MsgBoxPush(6);
+            while (1) {
+                fillArea(40, 85, 300, 105, COLOR_WHITE);
+                int x = 40, y = 25;
+                PrintMini(&x, &y, "Items:", 0, LCD_WIDTH_PX, 0, 0, COLOR_BLUE, COLOR_WHITE, 1, 0);
+                y = 45;
+
+                x = 40;
+                PrintMini(&x, &y, "Name", 0, LCD_WIDTH_PX, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
+                x = 152;
+                PrintMini(&x, &y, "Len", 0, LCD_WIDTH_PX, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
+                x = 222;
+                PrintMini(&x, &y, "Flags", 0, LCD_WIDTH_PX, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);
+                for (int i = 0; i < visible_lines && scroll + i < total_lines; i++) {
+                    int y = 70 + i * 20;
+                    int x = 40;
+                    PrintMini(&x, &y, item_display[scroll + i].name, 0, LCD_WIDTH_PX, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);  // Name
+                    x = 152;
+                    PrintMini(&x, &y, item_display[scroll + i].length, 0, LCD_WIDTH_PX, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);  // Length
+                    x = 222;
+                    PrintMini(&x, &y, item_display[scroll + i].flags, 0, LCD_WIDTH_PX, 0, 0, COLOR_BLACK, COLOR_WHITE, 1, 0);  // Flags
+                }
+
+                sb.indicatorpos = scroll;
+                Scrollbar(&sb);
+
+                GetKey(&key);
+                if (key == KEY_CTRL_EXIT) {
+                    break;
+                }
+                if (key == KEY_CTRL_DOWN && scroll < total_lines - visible_lines)
+                    scroll++;
+
+                if (key == KEY_CTRL_UP && scroll > 0) {
+                    scroll--;
+                }
+            }
+        }
+        MsgBoxPop();
     }
 }
--- src/deleteSingleDir.h
+++ src/deleteSingleDir.h
@@ -1,6 +1,12 @@
 #ifndef MCS_DELETESINGLEDIR_H
 #define MCS_DELETESINGLEDIR_H
 
+typedef struct {
+    char name[9];
+    char length[12];
+    char flags [12];
+} ItemDisplay;
+
 void deleteSingleDir();
 
 #endif
